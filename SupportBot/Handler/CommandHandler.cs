da5811a54c651485afd9187b35be6cc9368e227d
Planterror <47764567+Plantterror@users.noreply.github.com>
2020-08-09 12:30:28 -0600

    ping admins if bot breaks

--- SupportBot/Handler/CommandHandler.cs
+++ SupportBot/Handler/CommandHandler.cs
@@ -1,3 +1,4 @@
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
@@ -43,8 +44,14 @@ namespace SupportBot.Handler
                 if (!result.IsSuccess)
                 {
                     Console.WriteLine("Error happened while executing Command: " + result.ErrorReason + " ServerId: " + context.Guild.Id);
+                    if (result.Error != (CommandError.BadArgCount | CommandError.UnknownCommand | CommandError.ObjectNotFound))
+					{
+                        await context.Guild.GetTextChannel((ulong)AdminChannelID).SendMessageAsync("An error was encountered while command executing:" + result.Error + " " + MentionUtils.MentionRole((ulong)AdminID));
+                    }
                 }
             }
         }
+        private long AdminID = 741727508306722849; //DELETE THIS
+        private long AdminChannelID = 741727264588169332;
     }
 }
