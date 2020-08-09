89e994cb7899677ec6da309c9b4c73fbca75715c
Planterror <47764567+Plantterror@users.noreply.github.com>
2020-08-09 10:40:31 -0600

    DISCORD WHYYYYYYY
    I JUST WANNA BAN WITHOUT HAVING TO PRUNEEEEEEEEE

--- SupportBot/Modules/ModerationModule.cs
+++ SupportBot/Modules/ModerationModule.cs
@@ -1,25 +1,43 @@
-﻿using System;
-using System.Net.Mime;
-using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
-using Discord;
+﻿using Discord;
 using Discord.Commands;
+using System.Threading.Tasks;
 
 namespace SupportBot.Modules
 {
-    public class ModerationModule : ModuleBase<SocketCommandContext>
-    {
-        //TODO: Add ban, kick etc.
+	public class ModerationModule : ModuleBase<SocketCommandContext>
+	{
+		//TODO: Add ban, kick etc. -Doing
 
-        [Command("restart"), Alias("r")]
-        [RequireOwner]
-        [Summary("Shuts down the bot")]
-        public async Task HelpAsync()
-        {
-            await Context.Client.SetStatusAsync(UserStatus.Invisible);
-            await Task.Delay(1000);
-            Program.StartBotAsync().GetAwaiter().GetResult();
-        }
-    }
+		[Command("restart"), Alias("r")]
+		[RequireOwner]
+		[Summary("Restarts the bot")]
+		public async Task HelpAsync()
+		{
+			await Context.Client.SetStatusAsync(UserStatus.Invisible);
+			await Task.Delay(1000);
+			Program.StartBotAsync().GetAwaiter().GetResult();
+		}
+
+		[Command("kick")]
+		[RequireUserPermission(GuildPermission.KickMembers)]
+		[Summary("Kicks a user mentioned.")]
+		public async Task KickAsync(IGuildUser user, [Remainder] string reason = "No reason specified.")
+		{
+			await user.KickAsync(reason);
+			IUserMessage MessageToDelete = await ReplyAsync("User " + user.Username + " has been kicked. Reason: " + reason);
+			await Task.Delay(2500);
+			await MessageToDelete.DeleteAsync();
+		}
+		[Command("ban")]
+		[RequireUserPermission(GuildPermission.BanMembers)]
+		[Summary("Bans a user mentioned.")]
+		public async Task BanAsync(IGuildUser user, int daystoprune = 2, [Remainder] string reason = "No reason specified.")
+		{
+			
+			await user.BanAsync(daystoprune, reason);
+			IUserMessage MessageToDelete = await ReplyAsync("User " + user.Username + " has been banned. Reason: " + reason);
+			await Task.Delay(5000);
+			await MessageToDelete.DeleteAsync();
+		}
+	}
 }
