fc89447ec367fb25dc8d56c99426ce318e195b2d
TheStachelfisch <benbeschow@hotmail.com>
2021-03-04 08:59:16 +0100

    bad word done

--- tMod64Bot/Modules/BadWordModule.cs
+++ tMod64Bot/Modules/BadWordModule.cs
@@ -1,26 +1,62 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using tMod64Bot.Modules.Commons;
+using tMod64Bot.Utils;
 
 namespace tMod64Bot.Modules
 {
-    [Group("bw"), Alias("badword")]
+    [Group("badword"), Alias("bw", "bannedword")]
     [BotManagementPerms]
     public class BadWordModule : CommandBase
     {
-        public async Task AddWord(string word)
+        [Command("add")]
+        public async Task AddWord([Remainder]string word)
         {
-            
+            if (ConfigService.Config.BannedWords.Contains(word))
+            {
+                await ReplyAsync(embed: EmbedHelper.ErrorEmbed($"Bad word list already contains {word}"));
+                return;
+            }
+
+            ConfigService.Config.BannedWords.Add(word);
+            ConfigService.SaveData();
+            await ReplyAsync(embed: EmbedHelper.SuccessEmbed($"Successfully added {word} to the banned word list"));
         }
 
-        public async Task RemoveWord(string word)
+        [Command("remove")]
+        public async Task RemoveWord([Remainder]string word)
         {
-            
+            if (!ConfigService.Config.BannedWords.Contains(word))
+            {
+                await ReplyAsync(embed: EmbedHelper.ErrorEmbed($"Bad word list doesn't contain {word}"));
+                return;
+            }
+
+            ConfigService.Config.BannedWords.Remove(word);
+            ConfigService.SaveData();
+            await ReplyAsync(embed: EmbedHelper.SuccessEmbed($"Successfully removed '{word}' from the banned word list"));
         }
 
+        [Command("words"), Alias("get", "list")]
         public async Task GetWords()
         {
-            
+            if (!ConfigService.Config.BannedWords.Any())
+            {
+                await ReplyAsync(embed: EmbedHelper.ErrorEmbed("No banned words have been added yet"));
+                return;
+            }
+
+            EmbedBuilder embed = new()
+            {
+                Title = "Banned Words",
+                Color = Color.Green,
+                Description = $"**{ConfigService.Config.BannedWords.Count} {(ConfigService.Config.BannedWords.Count == 1 ? "word" : "words")}**\n```{String.Join(',', ConfigService.Config.BannedWords)}```",
+            };
+
+            await ReplyAsync(embed: embed.Build());
         }
     }
 }
