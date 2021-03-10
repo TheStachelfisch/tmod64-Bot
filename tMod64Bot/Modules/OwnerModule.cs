7b31fe52908d3d8b8ceb1a51065a6f35ae3aa81f
TheStachelfisch <benbeschow@hotmail.com>
2021-03-10 21:33:35 +0100

    Temporary test update command

---
+++ tMod64Bot/Modules/OwnerModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace tMod64Bot.Modules
+{
+    [RequireOwner]
+    public class OwnerModule : ModuleBase
+    {
+        [Command("update")]
+        public async Task Update()
+        {
+            await ReplyAsync("Test");
+        }
+    }
+}
