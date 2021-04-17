4de8f11c772897441e45cfed27eae7492bf5e037
TheStachelfisch <TheStachelfisch@users.noreply.github.com>
2021-04-17 16:22:48 +0200

    Fix perma-mute

--- tMod64Bot/Modules/ModerationModule.cs
+++ tMod64Bot/Modules/ModerationModule.cs
@@ -127,7 +127,6 @@ namespace tMod64Bot.Modules
         [Command("mute")]
         [RequireBotPermission(GuildPermission.ManageRoles)]
         [RequireUserPermission(GuildPermission.ManageRoles)]
-        [Priority(5)]
         public async Task MuteAsync([RequiresHierarchy]SocketGuildUser user, string? duration = null, [Remainder] string reason = "No Reason provided")
         {
             var span = duration == null ? TimeSpan.Zero : ModerationService.GetTimeSpan(duration);
@@ -164,6 +163,7 @@ namespace tMod64Bot.Modules
         [Command("mute")]
         [RequireBotPermission(GuildPermission.ManageRoles)]
         [RequireUserPermission(GuildPermission.ManageRoles)]
+        [Priority(5)]
         public async Task MuteAsync([RequiresHierarchy] SocketGuildUser user, [Remainder] string reason = "No Reason provided") 
             => await MuteAsync(user, null, reason);
 
