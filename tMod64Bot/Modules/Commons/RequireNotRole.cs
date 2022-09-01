f172fd1fac6ec649f10ecb854d0f1a8df9a72780
Planterror <aspergers657@gmail.com>
2022-09-01 09:57:18 -0600

    RequireNotRole Attribute
    Used to deny a user the command if they have a specific role.

---
+++ tMod64Bot/Modules/Commons/RequireNotRole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using tMod64Bot.Services.Config;
+
+namespace tMod64Bot.Modules.Commons
+{
+	internal class RequireNotRole : PreconditionAttribute
+	{
+		public ulong Role { get; set; }
+		public RequireNotRole(ulong role)
+		{
+			Role = role;
+		}
+
+		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+		{
+			var config = services.GetRequiredService<ConfigService>();
+
+			var user = context.User as SocketGuildUser;
+
+			var result = !user.Roles.ToList().Contains(context.Guild.GetRole(Role));
+			return Task.FromResult(result ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("Your roles deny you from using this command"));
+		}
+	}
+}
